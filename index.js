58011ed7159b31af7f6d57bab672348099404a42
KGameX <kgamex.vk@gmail.com>
2026-04-14 17:17:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use('/user', userRoutes)
 app.use('/question', questionRoutes)
 app.use('/answer', answerRoutes)
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'kgamex.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.listen(3000, function(err) {
 	if (err) console.log(err)
 	console.log("Server is listening on port " + 3000)
